b2330c626c8d7a1d42b6746402ad5e848cd6db2b
Paul <quazantip@gmail.com>
2023-04-12 22:46:52 -0500

    :bricks: Enum created class

---
+++ src/java/model/CurrencyEnum.java
@@ -0,0 +1,23 @@
+package src.java.model;
+
+public enum CurrencyEnum {
+
+    CAD(124, "CAD");
+
+
+    private final int intCode;
+    private final String letterCode;
+
+    public int getIntCode() {
+        return intCode;
+    }
+
+    public String getLetterCode() {
+        return letterCode;
+    }
+
+    CurrencyEnum(int intCode, String letterCode) {
+        this.intCode = intCode;
+        this.letterCode = letterCode;
+    }
+}
